b2073747752ae3788dc2611e470e0b0efd689b62
vipin yadav <130680245+vipinyadav01@users.noreply.github.com>
2024-10-16 00:13:30 +0530

    fix navbar and slider

--- Public/js/nav.js
+++ Public/js/nav.js
@@ -1,24 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-links");
 
-  burger.addEventListener("click", () => {
-    // Toggle Nav
-    nav.classList.toggle("nav-active");
+const navLinks = document.querySelectorAll(".nav-links li");
 
-    // Animate Links
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease-in-out forwards ${
-          index / 7 + 0.5
-        }s`;
-      }
-    });
+burger.addEventListener("click", () => {
+  // Toggle Nav
+nav.classList.toggle("nav-active")
 
-    // Burger Animation
-    burger.classList.toggle("toggle");
+  navLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease-in-out forwards ${
+        index / 7 + 0.5
+      }s`;
+    }
   });
+
+  // Burger Animation
+  burger.classList.toggle("toggle");
 });
