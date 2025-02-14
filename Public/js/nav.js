4a370c8b0326904de9348060fd22801db97f4668
vipin yadav <130680245+vipinyadav01@users.noreply.github.com>
2025-02-14 21:38:42 +0530

    add animation to navigation links for improved user experience

--- Public/js/nav.js
+++ Public/js/nav.js
@@ -1,6 +1,11 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
 const navLinks = document.querySelectorAll('.nav-links li');
+
+// Add animation to nav links
+navLinks.forEach((link, index) => {
+    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+});
 const body = document.body;
 
 function toggleNav() {
